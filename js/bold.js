30361cde1558d55fc4855074f26ffb2cfbe5d896
fattahsamit <fattahsamit@gmail.com>
2023-02-14 22:29:05 +0600

    bold button to make text bold

---
+++ js/bold.js
@@ -0,0 +1,8 @@
+const bold = document.getElementById("bold");
+bold.addEventListener("click", function () {
+  console.log("bold");
+  bold.style.backgroundColor = "red";
+
+  const faefaefea = text.classList.add("font-bold");
+  text.value = faefaefea;
+});
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+const text = document.getElementById("text-area");
+
+function getText() {
+  return text.value;
+}
